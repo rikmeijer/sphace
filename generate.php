a90c9e35ea9be5643978b4258c714447c08a0d4d
Rik Meijer <rikmeijer@gmail.com>
2016-07-01 20:10:43 +0200

    2: browsing
    make use of support for layouts
    
    Task-Url: http://github.com/rikmeijer/sphace/issues/2

--- generate.php
+++ generate.php
@@ -1,10 +1,22 @@
 <?php
 use Sphace\GUI\Window;
+use Sphace\GUI\Layout\Fixed;
+use Sphace\GUI\Layout\Plain;
 
 require __DIR__ . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 
 $windowDescription = json_decode(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "public.json"), false);
 
-$window = new Window($windowDescription->title);
+switch ($windowDescription->layout[0]) {
+    case 'fixed':
+        $layout = new Fixed($windowDescription->layout[1]->width);
+        break;
+        
+    default:
+        $layout = new Plain();
+        break;
+}
+
+$window = new Window($windowDescription->title, $layout);
 
 echo file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . "public" . DIRECTORY_SEPARATOR . "index.html", $window->render());
